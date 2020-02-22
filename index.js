66ad1525a0f06d6c054efc54222b205ac0c2bb5b
Tina Hang <mstinahang@gmail.com>
2020-02-22 23:17:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ class Tree {
     return 'A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.';
   }
 }
+
+class Deciduous extends Tree {
+  
+}
+
+class Evergreen extends Tree {
+  
+}
