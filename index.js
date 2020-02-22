120e3bd6b71a8cd74939569d29bb78caef7e16bb
Tina Hang <mstinahang@gmail.com>
2020-02-22 23:19:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class Deciduous extends Tree {
   }
 
   static definition() {
-    return super.definition() + 'Deciduous trees shed their leaves annually.'
+    return super.definition() + ' Deciduous trees shed their leaves annually.'
   }
 }
 
